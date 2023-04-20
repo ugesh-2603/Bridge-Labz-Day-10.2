d9923ecc638a34962fab9f1520c5da452b2a6190
Ugesh Kumar KS <ugesh262000@gmail.com>
2023-04-20 16:35:30 +0530

    Added uc2 Branch

--- flipcoin.sh
+++ flipcoin.sh
@@ -1,7 +1,16 @@
-flip=$((RANDOM % 2))
+heads=0
+tails=0
+flips=10
 
-if [ $flip -eq 0 ]; then
-  echo "Heads"
-else
-  echo "Tails"
-fi
+for ((i=1; i<=flips; i++)); do
+  flip=$((RANDOM % 2))
+
+  if [ $flip -eq 0 ]; then
+    heads=$((heads + 1))
+  else
+    tails=$((tails + 1))
+  fi
+done
+
+echo "Heads: $heads"
+echo "Tails: $tails"
